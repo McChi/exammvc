39f926627b194e6fb2da6858bd7cc210a852e5e6
baishucong <1015289039@qq.com>
2020-04-12 16:48:34 +0800

    将LoginMapper里的sql移到xml文件中

--- src/main/java/com/sk/exammvc/dao/LoginMapper.java
+++ src/main/java/com/sk/exammvc/dao/LoginMapper.java
@@ -17,7 +17,6 @@ public interface LoginMapper {
      * @param password 管理员密码
     * @return com.sk.exammvc.entity.Admin
     **/
-    @Select("select adminId,adminName,role from admin where adminId = #{username} and pwd = #{password}")
     Admin adminLogin(Integer username, String password);
 
     /**
@@ -27,8 +26,6 @@ public interface LoginMapper {
      * @param password 教师密码
     * @return com.sk.exammvc.entity.Teacher
     **/
-    @Select("select teacherId,teacherName,institute,sex,tel,email,cardId," +
-            "type,role from teacher where teacherId = #{username} and pwd = #{password}")
     Teacher teacherLogin(Integer username, String password);
 
     /**
@@ -38,7 +35,5 @@ public interface LoginMapper {
      * @param password 学生密码
     * @return com.sk.exammvc.entity.Student
     **/
-    @Select("select studentId,studentName,grade,major,clazz,institute,tel," +
-            "cardId,sex,role from student where studentId = #{username} and pwd = #{password}")
     Student studentLogin(Integer username, String password);
 }
