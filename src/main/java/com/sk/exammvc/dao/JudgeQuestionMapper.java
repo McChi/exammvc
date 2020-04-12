80ee21b440026dc03039c8f26038a9f959fc91b6
baishucong <1015289039@qq.com>
2020-04-12 17:56:13 +0800

    将选择、填空、判断的Mapper里的sql移到xml文件中

--- src/main/java/com/sk/exammvc/dao/JudgeQuestionMapper.java
+++ src/main/java/com/sk/exammvc/dao/JudgeQuestionMapper.java
@@ -3,8 +3,6 @@ package com.sk.exammvc.dao;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.sk.exammvc.entity.JudgeQuestion;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,7 +17,6 @@ public interface JudgeQuestionMapper {
     * @param paperId 试卷号
     * @return java.util.List<com.sk.exammvc.entity.JudgeQuestion>
     **/
-    @Select("select * from judge_question where questionId in (select questionId from paper_manage where questionType = 3 and paperId = #{paperId})")
     List<JudgeQuestion> findByIdAndType(Integer paperId);
 
     /**
@@ -28,7 +25,6 @@ public interface JudgeQuestionMapper {
     * @param page 分页对象
     * @return com.baomidou.mybatisplus.core.metadata.IPage<com.sk.exammvc.entity.JudgeQuestion>
     **/
-    @Select("select * from judge_question")
     IPage<JudgeQuestion> findAll(Page page);
 
     /**
@@ -36,7 +32,6 @@ public interface JudgeQuestionMapper {
      *
      * @return JudgeQuestion
      */
-    @Select("select questionId from judge_question order by questionId desc limit 1")
     JudgeQuestion findOnlyQuestionId();
 
     /**
@@ -45,17 +40,14 @@ public interface JudgeQuestionMapper {
     * @param judgeQuestion 判断题信息
     * @return int
     **/
-    @Insert("insert into judge_question(subject,question,answer,analysis,level,section) values " +
-            "(#{subject},#{question},#{answer},#{analysis},#{level},#{section})")
     int add(JudgeQuestion judgeQuestion);
 
     /**
-    * 根据科目和页码查询判断题
+    * 根据科目和题号查询判断题
     *
     * @param subject 科目
-     * @param pageNo 页码
+     * @param pageNo 题号
     * @return java.util.List<java.lang.Integer>
     **/
-    @Select("select questionId from judge_question  where subject=#{subject}  order by rand() desc limit #{pageNo}")
     List<Integer> findBySubject(String subject, Integer pageNo);
 }
--- src/main/java/com/sk/exammvc/dao/MultiQuestionMapper.java
+++ src/main/java/com/sk/exammvc/dao/MultiQuestionMapper.java
@@ -3,9 +3,7 @@ package com.sk.exammvc.dao;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.sk.exammvc.entity.MultiQuestion;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -20,7 +18,6 @@ public interface MultiQuestionMapper {
     * @param PaperId 试卷号
     * @return java.util.List<com.sk.exammvc.entity.MultiQuestion>
     **/
-    @Select("select * from multi_question where questionId in (select questionId from paper_manage where questionType = 1 and paperId = #{paperId})")
     List<MultiQuestion> findByIdAndType(Integer PaperId);
 
     /**
@@ -29,7 +26,6 @@ public interface MultiQuestionMapper {
     * @param page 分页对象
     * @return com.baomidou.mybatisplus.core.metadata.IPage<com.sk.exammvc.entity.MultiQuestion>
     **/
-    @Select("select * from multi_question")
     IPage<MultiQuestion> findAll(Page page);
 
     /**
@@ -37,7 +33,6 @@ public interface MultiQuestionMapper {
      *
      * @return MultiQuestion
      */
-    @Select("select questionId from multi_question order by questionId desc limit 1")
     MultiQuestion findOnlyQuestionId();
 
     /**
@@ -47,18 +42,15 @@ public interface MultiQuestionMapper {
     * @return int
     **/
     @Options(useGeneratedKeys = true,keyProperty = "questionId")
-    @Insert("insert into multi_question(subject,question,answerA,answerB,answerC,answerD,rightAnswer,analysis,section,level) " +
-            "values(#{subject},#{question},#{answerA},#{answerB},#{answerC},#{answerD},#{rightAnswer},#{analysis},#{section},#{level})")
     int add(MultiQuestion multiQuestion);
 
     /**
-    * 根据科目和页码查询选择题
+    * 根据科目和题号查询选择题
     *
     * @param subject 科目
-     * @param pageNo 页码
+     * @param pageNo 题号
     * @return java.util.List<java.lang.Integer>
     **/
-    @Select("select questionId from multi_question  where subject =#{subject} order by rand() desc limit #{pageNo}")
     List<Integer> findBySubject(String subject, Integer pageNo);
 
 
